f7874c225646f83f3af235d6056465216d3c8291
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2023-05-18 21:10:21 +0700

    update

--- Buoi9 - CP2/clock.js
+++ Buoi9 - CP2/clock.js
@@ -1,71 +1,83 @@
-class Clock {
-    $date
-    constructor(min, sec) {
-        this.$date = new Date(2023, 1, 1, 0, min, sec);
-    }
+// class Clock {
+//     $date
+//     constructor(min, sec) {
+//         this.$date = new Date(2023, 1, 1, 0, min, sec);
+//     }
 
-    getSecond() {
-        return this.$date.getSeconds();
-    }
+//     getSecond() {
+//         return this.$date.getSeconds();
+//     }
 
-    getMinute() {
-        return this.$date.getMinutes();
-    }
+//     getMinute() {
+//         return this.$date.getMinutes();
+//     }
 
-    setSecond(second) {
-        this.$date.setSeconds(second);
-    }
+//     setSecond(second) {
+//         this.$date.setSeconds(second);
+//     }
 
-    setMinute(minute) {
-        this.$date.setMinutes(minute);
-    }
+//     setMinute(minute) {
+//         this.$date.setMinutes(minute);
+//     }
+//     handleStartBtn = setInterval(this.start, 1000);
+//     start() {
+//         this.setSecond(this.getSecond() + 1);
+//     }
 
-    start = setInterval(
-        this.setSecond(1), 1000);
-
-    // start(index) {
-
-    //     console.log(index);
-    // }
-
-    stop(index) {
-        //  clearInterval(handleStartBtn);
-        console.log(index);
-    }
-}
-
+//     stop() {
+//         clearInterval(handleStartBtn);
+//         this.setSecond(0);
+//         this.setMinute(0);
+//     }
+// }
 //----- Create data--------
 const clockList = [
-    new Clock(2, 3),
-    new Clock(4, 59),
-    new Clock(10, 8),
-    new Clock(1, 19),
-    new Clock(0, 0)
+    { "index": 1, "date": new Date(2023, 1, 1, 0, 2, 3) },
+    { "index": 1, "date": new Date(2023, 1, 1, 0, 2, 3) },
+    { "index": 1, "date": new Date(2023, 1, 1, 0, 2, 3) },
+    { "index": 1, "date": new Date(2023, 1, 1, 0, 2, 3) }
 ];
+
+//-------- Start ------------
+const start = setInterval(function () {
+    const element = clockList.filter(clock => clock.index == 0);
+    console.log(element);
+    element.getSeconds
+}, 1000);
 
 //------- Code font end ----------
 const ol = document.getElementById('clock-list');
+function setFontend() {
+    for (let index = 0; index < clockList.length; index++) {
+        const element = clockList[index];
 
-for (let index = 0; index < clockList.length; index++) {
-    const element = clockList[index];
+        const li = document.createElement('li');
 
-    const li = document.createElement('li');
+        let time = element.getMinute() + ":" + element.getSecond();
 
-    let time = element.$min + ":" + element.$sec;
+        const startbtn = document.createElement('button');
+        startbtn.innerHTML = "Start";
+        startbtn.addEventListener("click", element.handleStartBtn);
 
-    const startbtn = document.createElement('button');
-    startbtn.innerHTML = "Start";
-    startbtn.addEventListener("click", element.start(index));
+        const stopbtn = document.createElement('button');
+        stopbtn.innerHTML = "Stop";
+        stopbtn.addEventListener("click", element.stop);
 
-    const stopbtn = document.createElement('button');
-    stopbtn.innerHTML = "Stop";
-    stopbtn.addEventListener("click", element.stop(index));
-
-    li.innerHTML = time;
-    li.appendChild(startbtn);
-    li.appendChild(stopbtn);
-    ol.appendChild(li);
+        li.innerHTML = time;
+        li.appendChild(startbtn);
+        li.appendChild(stopbtn);
+        ol.appendChild(li);
+    }
 }
 
+setFontend();
+
+//----- Stop all ---------
+function stopAll() {
+    for (let index = 0; index < clockList.length; index++) {
+        clockList[index] = new Clock(0, 0);
+    }
+    setFontend();
+}
 
 
